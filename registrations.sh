c1582fc36f65fda75f878276cf1cc8812864fe06
Kirstie Whitaker <kw401@cam.ac.uk>
2013-07-03 18:03:42 +0100

    still going

--- registrations.sh
+++ registrations.sh
@@ -14,8 +14,8 @@
 #                      DTI, FSL_highres, Freesurfer, MNI152.
 #
 #              USAGE:  registrations.sh <dti_data_folder> <mprage_data_folder>
-#                           eg: registrations.sh ${dti_dir} ${mprage_dir}
-#                           eg: registrations.sh /home/kw401/MRIMPACT/ANALYSES/1106/t1/DTI /home/kw401/MRIMPACT/ANALYSES/1106/t1/MPRAGE
+#                           eg: registrations.sh ${dti_dir} ${mprage_dir} ${subid}
+#                           eg: registrations.sh /home/kw401/MRIMPACT/ANALYSES/1106/t1/DTI /home/kw401/MRIMPACT/ANALYSES/1106/t1/MPRAGE 1106t1
 #
 #        PARAMETER 1:  DTI data folder (full path)
 #                           If you're using this script as part of another
@@ -29,6 +29,10 @@
 #                           If you're using this script alone
 #                               eg: /home/kw401/MRIMPACT/ANALYSES/1106/t1/MPRAGE
 #
+#        PARAMETER 3:  subject ID
+#                           eg: ${subid}
+#                           eg: 1106t1
+#
 #             AUTHOR:  Kirstie Whitaker
 #                          kw401@cam.ac.uk
 #
@@ -39,9 +43,9 @@
 # Define usage function
 function usage {
     echo "USAGE:"
-    echo "registrations.sh <dti_data_folder> <mprage_data_folder>"
-    echo "    eg: registrations.sh \${dti_dir} \${mprage_dir}"
-    echo "    eg: registrations.sh /home/kw401/MRIMPACT/ANALYSES/1106/t1/DTI /home/kw401/MRIMPACT/ANALYSES/1106/t1/MPRAGE"
+    echo "registrations.sh <dti_data_folder> <mprage_data_folder> <subid>"
+    echo "    eg: registrations.sh \${dti_dir} \${mprage_dir} \${subid}"
+    echo "    eg: registrations.sh /home/kw401/MRIMPACT/ANALYSES/1106/t1/DTI /home/kw401/MRIMPACT/ANALYSES/1106/t1/MPRAGE 1106t1"
     exit
 }
 #------------------------------------------------------------------------------
@@ -60,6 +64,7 @@ fi
 
 surf_dir=${mprage_dir}/SURF/
 
+sub=$3
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
@@ -78,31 +83,44 @@ if [[ ! -d ${mprage_dir} ]]; then
     print_usage=1
 fi
 
+# Exit if subID is an empty string
+if [[ -z ${sub} ]]; then
+    echo "    SubID is blank"
+    print_usage=1
+fi
+
 # Print the usage if necessary
 if [[ ${print_usage} == 1 ]]; then
     usage
 fi
 
 ### Step 2: Check data
-# Make sure dti_ec_brain.nii.gz, highres_brain.nii.gz
-# orig.mgz files exist
-if [[ ! -f ${dti_dir}/dti_ec_brain.nii.gz ]]; then
-    echo "    No dti_ec_brain.nii.gz file"
-    echo "    Check that dti_preprocessing.sh has finished"
-    print_usage=1
-fi
+# Make sure dti_ec_brain.nii.gz, <subid>_FA.nii.gz, highres_brain.nii.gz,
+# highres.nii.gz, rawavg.mgz and orig.mgz files exist
+for dti_file in ${dti_dir}/dti_ec_brain.nii.gz \
+                    ${dti_dir}/FDT/${sub}_FA.nii.gz; do
+    if [[ ! -f ${dti_file} ]]; then
+        echo "    No `basename ${dti_file}` file"
+        echo "    Check that dti_preprocessing.sh has finished"
+        print_usage=1
+    fi
+done
 
-if [[ ! -f ${mprage_dir}/highres.nii.gz ]]; then
-    echo "    No highres.nii.gz file"
-    echo "    Check that mprage_processing.sh has finished"
-    print_usage=1
-fi
+for highres_file in ${mprage_dir}/highres.nii.gz ${mprage_dir}/highres_brain.nii.gz; do
+    if [[ ! -f ${highres_file} ]]; then
+        echo "    No `basename ${highres_file}` file"
+        echo "    Check that mprage_processing.sh has finished"
+        print_usage=1
+    fi
+done
 
-if [[ ! -f ${surf_dir}/mri/orig.mgz ]]; then
-    echo "    No orig.mgz file"
-    echo "    Check that mprage_processing.sh has finished"
-    print_usage=1
-fi
+for surf_file in ${surf_dir}/mri/rawavg.mgz ${surf_dir}/mri/orig.mgz ; do
+    if [[ ! -f ${surf_file} ]]; then
+        echo "    No `basename ${surf_file}` file"
+        echo "    Check that mprage_processing.sh has finished"
+        print_usage=1
+    fi
+done
 
 # Print the usage if necessary
 if [[ ${print_usage} == 1 ]]; then
@@ -114,27 +132,87 @@ fi
 # Get started
 echo "DTI_DIR: ${dti_dir}"
 echo "MPRAGE_DIR: ${mprage_dir}"
+echo "SURF_DIR: ${surf_dir}"
 
+# Define the registration directory
 reg_dir=(`dirname ${dti_dir}`)
+
 # Make the LOGS dir
 logdir=${reg_dir}/LOGS
 mkdir -p ${logdir}
 
 #------------------------------------------------------------------------------
-# Register diffusion 
-#------------------------------------------------------------------------------
-# Register to standard space
-# First flirt highres to MNI152
-if [[ ! -f ${mprage_dir}/highres_brain.nii.gz ]]; then
-    echo "    ERROR: Can't run registration because brain extraction has not been completed"
+# Register diffusion images to highres space using Flirt
+
+# b0 weighted file
+if [[ ! -f ${reg_dir}/diffB0_TO_highres.mat ]]; then
+    echo "    Flirting dti_ec_brain to highres"
+    flirt -ref ${mprage_dir}/highres_brain.nii.gz \
+            -in ${dti_dir}/dti_ec_brain.nii.gz \
+            -omat ${reg_dir}/diffB0_TO_highres.mat
+
+else
+    echo "    dti_ec_brain already flirted to highres"
+
+fi
+
+# Invert this flirt transform
+if [[ ! -f ${reg_dir}/diffB0_TO_highres.mat ]]; then
+    echo "    ERROR: Can't invert transform as flirt has not been completed"
     echo "    EXITING"
     exit
 
-elif [[ ! -f ${mprage_dir}/highres_TO_MNI152.mat ]]; then
+elif [[ ! -f ${reg_dir}/highres_TO_diffB0.mat ]]; then
+    echo "    Inverting flirt transform"
+    convert_xfm -omat ${reg_dir}/highres_TO_diffB0.mat \
+                -inverse ${reg_dir}/diffB0_TO_highres.mat
+
+else
+    echo "    Inverse flirt transform already calculated"
+
+fi
+
+# FA file
+if [[ ! -f ${reg_dir}/diffFA_TO_highres.mat ]]; then
+    echo "    Flirting <subid>_FA to highres"
+    flirt -ref ${mprage_dir}/highres_brain.nii.gz \
+            -in ${dti_dir}/FDT/${sub}_FA.nii.gz \
+            -omat ${reg_dir}/diffFA_TO_highres.mat
+
+else
+    echo "    dti_ec_brain already flirted to highres"
+
+fi
+
+# Invert this flirt transform
+if [[ ! -f ${reg_dir}/diffFA_TO_highres.mat ]]; then
+    echo "    ERROR: Can't invert transform as flirt has not been completed"
+    echo "    EXITING"
+    exit
+
+elif [[ ! -f ${reg_dir}/highres_TO_diffFA.mat ]]; then
+    echo "    Inverting flirt transform"
+    convert_xfm -omat ${reg_dir}/highres_TO_diffFA.mat \
+                -inverse ${reg_dir}/diffFA_TO_highres.mat
+
+else
+    echo "    Inverse flirt transform already calculated"
+
+fi
+
+#------------------------------------------------------------------------------
+# Register highres to MNI152 standard space
+# Save these in both the mprage_dir and the reg_dir so that you don't have
+# to re-run them if you're dealing with multiple dti directories
+# (This is for Kirstie's original use of comparing various dti acquisitions!)
+
+# Flirt first
+if [[ ! -f ${mprage_dir}/highres_TO_MNI152.mat ]]; then
     echo "    Flirting highres to MNI"
     flirt -ref ${FSLDIR}/data/standard/MNI152_T1_2mm_brain.nii.gz \
             -in ${mprage_dir}/highres_brain.nii.gz \
             -omat ${mprage_dir}/highres_TO_MNI152.mat
+    cp ${mprage_dir}/highres_TO_MNI152.mat ${reg_dir}/highres_TO_MNI152.mat
 
 else
     echo "    Highres already flirted to MNI"
@@ -142,7 +220,7 @@ else
 fi
 
 # Invert this flirt transform
-if [[ ! -f ${mprage_dir}/highres_TO_MNI152.nii.gz ]]; then
+if [[ ! -f ${reg_dir}/highres_TO_MNI152.mat ]]; then
     echo "    ERROR: Can't invert transform as flirt has not been completed"
     echo "    EXITING"
     exit
@@ -151,163 +229,136 @@ elif [[ ! -f ${mprage_dir}/MNI152_TO_highres.mat ]]; then
     echo "    Inverting flirt transform"
     convert_xfm -omat ${mprage_dir}/MNI152_TO_highres.mat \
                 -inverse ${mprage_dir}/highres_TO_MNI152.mat
+    cp ${mprage_dir}/MNI152_TO_highres.mat ${reg_dir}/MNI152_TO_highres.mat
 
 else
     echo "    Inverse flirt transform already calculated"
 
+fi
+
 # Then fnirt highres to MNI152
-if [[ ! -f ${mprage_dir}/highres_TO_MNI152_nlwarp.nii.gz ]]; then
-    echo "Fnirting highres to MNI"
+if [[ ! -f ${reg_dir}/highres_TO_MNI152_nlwarp.nii.gz ]]; then
+    echo "    Fnirting highres to MNI"
     fnirt --in=${mprage_dir}/highres.nii.gz \
             --aff=${mprage_dir}/highres_TO_MNI152.mat \
             --cout=${mprage_dir}/highres_TO_MNI152_nlwarp \
             --config=T1_2_MNI152_2mm
+
+    cp ${mprage_dir}/highres_TO_MNI152_nlwarp.nii.gz \
+        ${reg_dir}/highres_TO_MNI152_nlwarp.nii.gz
+
 else
     echo "    Highres already fnirted to MNI"
 
 fi
 
 # And inverse this warp
-if [[ ! -f ${mprage_dir}/highres_TO_MNI152_nlwarp.nii.gz ]]; then
+if [[ ! -f ${reg_dir}/highres_TO_MNI152_nlwarp.nii.gz ]]; then
     echo "    ERROR: Can't run registration because fnirt has not been completed"
     echo "    EXITING"
     exit
 
-elif [[ ! -f ${mprage_dir}/MNI152_TO_highres_nl.nii.gz ]]; then
-    echo "Inverting highres to MNI warp"
+elif [[ ! -f ${reg_dir}/MNI152_TO_highres_nlwarp.nii.gz ]]; then
+    echo "    Inverting highres to MNI warp"
     invwarp --ref=${mprage_dir}/highres.nii.gz \
-            --warp=${mprage_dir}/highres_TO_MNI152_nl.nii.gz \
-            --out=${mprage_dir}/MNI152_TO_highres_nl.nii.gz
+            --warp=${mprage_dir}/highres_TO_MNI152_nlwarp.nii.gz \
+            --out=${mprage_dir}/MNI152_TO_highres_nlwarp.nii.gz
+
+    cp ${mprage_dir}/MNI152_TO_highres_nlwarp.nii.gz \
+        ${reg_dir}/MNI152_TO_highres_nlwarp.nii.gz
+
 else
     echo "    Inverse fnirt warp already calculated"
 
 fi
 
-
-# First flirt high_res to MNI152
-if [[ ! -f ${mprage_dir}/high_res_TO_MNI152.mat ]]; then
-    echo "Flirting high_res to MNI"
-    flirt -ref ${FSLDIR}/data/standard/MNI152_T1_2mm_brain.nii.gz \
-            -in ${mprage_dir}/high_res_brain.nii.gz \
-            -omat ${mprage_dir}/high_res_TO_MNI152.mat
-fi
-
-# Then fnirt high_res to MNI152
-if [[ ! -f ${mprage_dir}/high_res_TO_MNI152_nl.nii.gz ]]; then
-    echo "Fnirting high_res to MNI"
-    fnirt --in=${mprage_dir}/high_res.nii.gz \
-            --aff=${mprage_dir}/high_res_TO_MNI152.mat \
-            --cout=${mprage_dir}/high_res_TO_MNI152_nl \
-            --config=T1_2_MNI152_2mm
-fi
-
-# And inverse this warp
-if [[ ! -f ${mprage_dir}/MNI152_TO_high_res_nl.nii.gz ]]; then
-    echo "Inverting high_res to MNI warp"
-    invwarp --ref=${mprage_dir}/high_res.nii.gz \
-            --warp=${mprage_dir}/high_res_TO_MNI152_nl.nii.gz \
-            --out=${mprage_dir}/MNI152_TO_high_res_nl.nii.gz
-fi
-
 #------------------------------------------------------------------------------
-# Rotate bvecs
+# Register highres to freesurfer space
 
-if [[ ! -f ${dir}/bvecs ]]; then
-    echo "    Rotating bvecs"
-    ${rot_bvecs_script} ${dir}/bvecs_orig ${dir}/bvecs \
-        ${dir}/dti_ec.ecclog >> ${logdir}/eddycorrect
-else
-    echo "    Bvecs already rotated"
-fi
+if [[ ! -f ${reg_dir}/freesurfer_TO_highres.mat ]]; then
+    echo "    Registering highres to freesurfer space"
 
-#------------------------------------------------------------------------------
-# Brain extract
-if [[ ! -f ${dir}/dti_ec.nii.gz ]]; then
-    echo "    ERROR: Can't brain extract because eddy_correct has not been completed"
-    echo "    EXITING"
-    exit
+    tkregister2 --mov ${surf_dir}/mri/orig.mgz \
+                --targ ${surf_dir}/mri/rawavg.mgz \
+                --regheader \
+                --reg junk \
+                --fslregout ${mprage_dir}/freesurfer_TO_highres.mat \
+                --noedit 
 
-elif [[ ! -f ${dir}/dti_ec_brain.nii.gz ]]; then
-    echo "    Brain extracting"
-    bet ${dir}/dti_ec.nii.gz ${dir}/dti_ec_brain.nii.gz -f 0.15 -m > ${logdir}/bet
+    cp ${mprage_dir}/freesurfer_TO_highres.mat \
+        ${reg_dir}/freesurfer_TO_highres.mat
 
 else
-    echo "    Brain already extracted"
+    echo "    Highres already registered to freesurfer space"
+
 fi
 
-#------------------------------------------------------------------------------
-# DTIfit (FDT)
-if [[ ! -f ${dir}/dti_ec_brain_mask.nii.gz || ! -f ${dir}/bvecs ]]; then
-    echo "    ERROR: Can't fit tensor because brain extraction has not been completed"
+if  [[ ! -f ${reg_dir}/freesurfer_TO_highres.mat ]]; then
+    echo "    ERROR: Can't run registration because tkregister2 hasn't been completed"
     echo "    EXITING"
     exit
 
-elif [[ ! -f ${dir}/bvecs ]]; then
-    echo "    ERROR: Can't fit tensor because bvecs file doesn't exist"
-    echo "    EXITING"
-    exit
-
-elif [[ ! -f ${dir}/FDT/${sub}_MO.nii.gz ]]; then
-    echo "    Fitting tensor"
-    mkdir -p ${dir}/FDT
-    dtifit -k ${dir}/dti_ec.nii.gz \
-        -m ${dir}/dti_ec_brain_mask.nii.gz \
-        -r ${dir}/bvecs \
-        -b ${dir}/bvals \
-        -o ${dir}/FDT/${sub} \
-        > ${logdir}/dtifit
+elif [[ ! -f ${reg_dir}/highres_TO_freesurfer.mat ]]; then
+    echo "    Inverting freesurfer to highres transform"
     
-    fslmaths ${dir}/FDT/${sub}_L2.nii.gz -add ${dir}/FDT/${sub}_L3.nii.gz -div 2 \
-        ${dir}/FDT/${sub}_L23.nii.gz
+    convert_xfm -omat ${mprage_dir}/highres_TO_freesurfer.mat \
+                -inverse ${mprage_dir}/freesurfer_TO_highres.mat 
+
+    cp ${mprage_dir}/highres_TO_freesurfer.mat \
+        ${reg_dir}/highres_TO_freesurfer.mat
 
 else
-   echo "    Tensor already fit"
+    echo "    Inverse freesurfer to highres transform already calculated"
+
 fi
+
 
 #------------------------------------------------------------------------------
-# BedpostX
-if [[ ! -f ${dir}/BEDPOSTX.bedpostX/dyads2.nii.gz ]]; then
-    echo "    Now starting bedpostX"
-    mkdir -p ${dir}/BEDPOSTX
-    cp ${dir}/bvals ${dir}/BEDPOSTX/
-    cp ${dir}/bvecs ${dir}/BEDPOSTX/
-    cp ${dir}/dti_ec_brain_mask.nii.gz \
-    ${dir}/BEDPOSTX/nodif_brain_mask.nii.gz
-    cp ${dir}/dti_ec.nii.gz ${dir}/BEDPOSTX/data.nii.gz
-    bedpostx ${dir}/BEDPOSTX/ > ${logdir}/bedpostx
+# Concatenate the diffusion and highres registrations
+if [[ ! -f ${reg_dir}/MNI152_TO_diffFA.mat ]]; then
+    echo "    Concatenating and inverting remaining transforms"
+
+    # diffB0 to freesurfer
+    convert_xfm -omat ${reg_dir}/diffB0_TO_freesurfer.mat \
+                -concat ${reg_dir}/diffB0_TO_highres.mat \
+                        ${reg_dir}/highres_TO_freesurfer.mat 
+
+    # freesurfer to diffB0
+    convert_xfm -omat ${reg_dir}/freesurfer_TO_diffB0.mat \
+                -inverse ${reg_dir}/diffB0_TO_freesurfer.mat
+
+    # diffFA to freesurfer
+    convert_xfm -omat ${reg_dir}/diffFA_TO_freesurfer.mat \
+                -concat ${reg_dir}/diffFA_TO_highres.mat \
+                        ${reg_dir}/highres_TO_freesurfer.mat 
+
+    # freesurfer to diffFA
+    convert_xfm -omat ${reg_dir}/freesurfer_TO_diffFA.mat \
+                -inverse ${reg_dir}/diffFA_TO_freesurfer.mat
+
+    # diffB0 to MNI152
+    convert_xfm -omat ${reg_dir}/diffB0_TO_MNI152.mat \
+                -concat ${reg_dir}/diffB0_TO_highres.mat \
+                        ${reg_dir}/highres_TO_MNI152.mat 
+
+    # MNI152 to diffB0
+    convert_xfm -omat ${reg_dir}/MNI152_TO_diffB0.mat \
+                -inverse ${reg_dir}/diffB0_TO_MNI152.mat
+
+    # diffFA to MNI152
+    convert_xfm -omat ${reg_dir}/diffFA_TO_MNI152.mat \
+                -concat ${reg_dir}/diffFA_TO_highres.mat \
+                        ${reg_dir}/highres_TO_MNI152.mat 
+
+    # MNI152 to diffB0
+    convert_xfm -omat ${reg_dir}/MNI152_TO_diffFA.mat \
+                -inverse ${reg_dir}/diffFA_TO_MNI152.mat
+            
 else
-    echo "    bedpostX already complete"
+    echo "    Remaining transforms already calculated"
+
 fi
 
-#------------------------------------------------------------------------------
-# TBSS 1 and 2
-if [[ ! -f ${dir}/FDT/${sub}_FA.nii.gz ]]; then
-    echo "    ERROR: Can't run TBSS as tensor has not been fit"
-    echo "    EXITING"
-    exit
-
-elif [[ ! -f ${dir}/TBSS/FA/reverse_fnirt_warp.nii.gz ]]; then
-    echo "    Now starting tbss"
-    if [[ ! -f ${dir}/TBSS/FA/${sub}_FA_FA_to_target_warp.nii.gz ]]; then
-        echo "    Running TBSS"
-        rm -rf ${dir}/TBSS
-        mkdir -p ${dir}/TBSS
-        cp ${dir}/FDT/*FA* ${dir}/TBSS/
-        cd ${dir}/TBSS/
-        tbss_1_preproc * > ${logdir}/tbss
-        tbss_2_reg -T >> ${logdir}/tbss
-    fi
-    # Now create the inverse fnirt warp
-    echo "    Inverting FNIRT warp"
-    if [[ -d ${dir}/TBSS/FA && \
-           ! -f ${dir}/TBSS/FA/reverse_fnirt_warp.nii.gz ]]; then
-        invwarp -r ${dir}/TBSS/FA/${sub}_FA_FA.nii.gz \
-                -w ${dir}/TBSS/FA/${sub}_FA_FA_to_target_warp.nii.gz \
-                -o ${dir}/TBSS/FA/reverse_fnirt_warp.nii.gz
-    fi
-else
-    echo "    TBSS already complete"
-fi
 
 #------------------------------------------------------------------------------
 # And you're done!
