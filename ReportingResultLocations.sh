0fa2d3973da131d86a61433cc4bfc481fe8e72cc
Kirstie Whitaker <kw401@cam.ac.uk>
2013-11-24 01:13:09 +0000

    forgot the unclassified option

--- ReportingResultLocations.sh
+++ ReportingResultLocations.sh
@@ -24,18 +24,18 @@ for input in $1 $2 $3 $4; do
     
 done
 
+# Figure out the atlas name
+atlas_name=`basename ${atlas} .nii.gz`
+
 result_locations="`basename ${result} .nii.gz`_`basename ${labels_file} .xml`.csv"
 # If the locations file already exists then don't run this script
 if [[ -f ${result_locations} ]]; then
-    echo "    Locations already reported"
+    echo "    ${atlas_name} locations already reported"
     exit
 fi
 
 # But if it doesn't then create it and write in the header
 echo "Atlas_label, N_voxels_result, N_voxels_skeleton" > ${result_locations}
-
-# Figure out the atlas name
-atlas_name=`basename ${atlas} .nii.gz`
 
 #==============================================================================
 # Lets start by thresholding the result for significant voxels only
@@ -47,7 +47,7 @@ result_thr_atlas_unclass=${result%.nii.gz}_thr95_${atlas_name}_unclassified.nii.
 
 fslmaths ${result} -thr 0.95 -bin ${result_thr_bin}
 fslmaths ${result_thr_bin} -mul ${atlas} ${result_thr_atlas}
-fslmaths ${result_thr_atlas} -bin -sub ${result_thr_bin} -mul -1 ${result_thr_unclass}
+fslmaths ${result_thr_atlas} -bin -sub ${result_thr_bin} -mul -1 ${result_thr_atlas_unclass}
 
 #==============================================================================
 # You also need to know how many voxels from the mean skeleton fall in the atlas regions
@@ -84,6 +84,12 @@ while [[ ${i} -lt ${atlas_max} ]]; do
     fi
     let i=${i}+1
 done
+
+# Finally add in the unclassified answer
+vol=(`fslstats ${results_thr_atlas_unclass} -V`)
+vol_skel=(`fslstats ${mean_skeleton_atlas_unclass} -V`)
+percent=(`echo "${vol}/${vol_skel} * 100" | bc -l`)
+echo "Unclassified, ${vol}, ${vol_skel}, ${percent}" >> ${result_locations}
 
 #==============================================================================
 # All done, well done ;)
