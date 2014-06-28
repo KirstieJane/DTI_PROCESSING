2845171ff347f9e3687c9fb3e7e4780afe607054
Kirstie Whitaker <kw401@cam.ac.uk>
2014-06-28 14:51:19 +0100

    got rid of argparser!

--- plot_connectivity_weights.py
+++ plot_connectivity_weights.py
@@ -16,60 +16,6 @@ import argparse
 # FUNCTIONS
 #=============================================================================
 
-# Set up the argparser so you can read arguments from the command line
-def setup_argparser():
-    '''
-    # Code to read in arguments from the command line
-    # Also allows you to change some settings
-    '''
-    
-    # Build a basic parser.
-    help_text = ('Create a histogram of weights from a connectivity matrix')
-    
-    sign_off = 'Author: Kirstie Whitaker <kw401@cam.ac.uk>'
-    
-    parser = argparse.ArgumentParser(description=help_text, epilog=sign_off)
-    
-    # Now add the arguments
-    # Required argument: dti_dir
-    parser.add_argument(dest='M_file', 
-                            type=str,
-                            metavar='M_file',
-                            help='Matrix (text file)')
-        
-    # Optional argument: minimum
-    parser.add_argument(dest='--hist_min', 
-                            type=float,
-                            metavar='hist_min',
-                            required=False,
-                            help='histogram minimum value',
-                            default=0.0)
-
-    # Optional argument: maximum
-    parser.add_argument(dest='--hist_max', 
-                            type=float,
-                            metavar='hist_max',
-                            required=False,
-                            help='histogram maximum value',
-                            default=300)
-                            
-    # Optional argument: color
-    parser.add_argument(dest='--hist_color', 
-                            type=str,
-                            metavar='histogram color',
-                            required=False,
-                            help='histogram color',
-                            default='SteelBlue')
-
-    # Optional argument: no_cost_box
-    parser.add_argument(dest='--no_cost_box', 
-                            help='do not show cost in text box',
-                            required=False,
-                            action='store_false')
-                            
-    arguments = parser.parse_args()
-    
-    return arguments, parser
 
 #=============================================================================
 # Define some variables
