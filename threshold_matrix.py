27d3449cbfc8913126556a4562d3d885da295b2b
Kirstie Whitaker <kw401@cam.ac.uk>
2014-06-28 23:26:23 +0100

    Revert "threshold change n_keep to cost"
    This reverts commit fdfbd4720f9a8eabcf81216024db5cdd5dbc1f47.

--- threshold_matrix.py
+++ threshold_matrix.py
@@ -38,10 +38,10 @@ def setup_argparser():
                             metavar='M_file',
                             help='Matrix (text file)')
         
-    # Required argument: cost
-    parser.add_argument('cost',
-                            type=float,
-                            help='target cost')
+    # Required argument: n_keep
+    parser.add_argument('n_keep',
+                            type=int,
+                            help='number of highest weights to keep **IN THE TOP TRIANGLE**')
                             
     arguments = parser.parse_args()
     
@@ -86,17 +86,13 @@ def threshold_Mtriu(M_triu, n_keep):
 arguments, parser = setup_argparser()
 
 M_file = arguments.M_file
-cost = arguments.cost
+n_keep = arguments.n_keep
 
 # Load in the matrix
 M = np.loadtxt(M_file)
 
 # Zero out the lower triangle and the diagonal
 M_triu = np.triu(M, 1)
-
-# Calculate the number of elements to keep
-n = M.shape[0]
-n_keep = cost * (n * (n-1)) * 0.5
 
 # Threshold M_triu
 thresh_M_triu = threshold_Mtriu(M_triu, n_keep)
