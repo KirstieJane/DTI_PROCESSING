7d277bb7bf10422fab0bb61fee0fb509e46fc348
Kirstie Whitaker <kw401@cam.ac.uk>
2014-07-17 11:38:03 +0100

    _list not list

--- create_average_mat.py
+++ create_average_mat.py
@@ -14,7 +14,6 @@ import argparse
 import os
 import sys
 
-
 #=============================================================================
 # FUNCTIONS
 #=============================================================================
@@ -84,7 +83,7 @@ def save_png(M, M_fig_name):
 arguments, parser = setup_argparser()
 M_file_list_file = arguments.M_file_list
 
-if not M_file_list_file.endswith('list'):
+if not M_file_list_file.endswith('_list'):
     print "M file list file needs to end with the word list"
     sys.exit
 
@@ -133,17 +132,17 @@ av_bin_M = av_bin_M / n
 
 # Save the matrices as text files
 #----- AVERAGE -------------------
-M_text_name = M_file_list_file.replace('list', '_avMat.txt')
+M_text_name = M_file_list_file.replace('_list', '_avMat.txt')
 save_mat(av_M, M_text_name)
 M_png_name = M_text_name.replace('.txt', '.png')
 save_png(av_M, M_png_name)
 #----- NORMALISE & AVERAGE -------
-M_text_name = M_file_list_file.replace('list', '_avNormMat.txt')
+M_text_name = M_file_list_file.replace('_list', '_avNormMat.txt')
 save_mat(av_norm_M, M_text_name)
 M_png_name = M_text_name.replace('.txt', '.png')
 save_png(av_norm_M, M_png_name)
 #----- BINARIZE & AVERAGE -------
-M_text_name = M_file_list_file.replace('list', '_avBinMat.txt')
+M_text_name = M_file_list_file.replace('_list', '_avBinMat.txt')
 save_mat(av_bin_M, M_text_name)
 M_png_name = M_text_name.replace('.txt', '.png')
 save_png(av_bin_M, M_png_name)
